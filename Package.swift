c8f81e4b398c77f4521786aab21bbd946e17abec
ArkadiGiniApps <arkadiy@gini-apps.com>
2024-05-06 11:18:34 +0300

    master - update package file

--- Package.swift
+++ Package.swift
@@ -3,25 +3,18 @@
 
 import PackageDescription
 
-let package = Package(name: "Joyned")
+let package = Package(name: "JoynedMobileSDK")
 package.swiftLanguageVersions = [.v5]
 package.platforms   = [
     .iOS(.v13),
 ]
 package.products    = [
     // Products define the executables and libraries a package produces, making them visible to other packages.
-    .library(name: "JoynedFramework", targets: ["JoynedBinaryWrapper"])
+    .library(name: "JoynedFramework", targets: ["JoynedFramework"])
 ]
 package.targets     = [
-    .target(
-        name: "JoynedBinaryWrapper",
-        dependencies: [
-            "JoynedBinary",
-        ],
-        path: "JoynedWrapper"
-    ),
     .binaryTarget(
-        name: "JoynedBinary",
+        name: "JoynedFramework",
         url: "https://github.com/gamitee-dev/ios-sdk-storage/releases/download/1.0.0/JoynedMobileSDK.xcframework.zip",
         checksum: "3d7d8836d6da75ed90b2394bae88ae897be77d9fac287c9a6cf0721ed1265533"
     ),
